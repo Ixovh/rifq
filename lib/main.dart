8f76c107a475fa7735b9fd5902344a19cc348404
Hatem Ayed <ixovhh@gmail.com>
2025-11-23 21:43:17 +0300

    uncomplete work becuse there is an update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rifq/core/di/setup.dart';
 import 'package:rifq/core/theme/app_theme.dart';
+import 'package:rifq/features/owner_flow/auth/presentation/pages/welcome_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,57 +16,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      // designSize: const Size(402, 874),
-      builder: (_, _) => MaterialApp(home: const HomePage()),
+      builder: (_, _) => MaterialApp(home: const WelcomeScreen()),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: context.background,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Primary Button',
-              style: context.body1.copyWith(color: context.neutral100),
-            ),
-            SizedBox(height: 16.h),
-            Text(
-              'Heading',
-              style: context.h1.copyWith(color: context.neutral100),
-            ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: context.background,
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               'Primary Button',
+//               style: context.body1.copyWith(color: context.neutral100),
+//             ),
+//             SizedBox(height: 16.h),
+//             Text(
+//               'Heading',
+//               style: context.h1.copyWith(color: context.neutral100),
+//             ),
 
-            FormBuilderTextField(
-              name: 'email',
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.construction),
-                prefixIcon: Icon(Icons.construction),
-                labelText: 'Email',
-                hintText: 'you@example.com',
-                filled: true,
-                fillColor: context.neutral200,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.r),
-                  borderSide: BorderSide(color: context.neutral300),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8.r),
-                  borderSide: BorderSide(color: context.primary300, width: 2),
-                ),
-              ),
-              style: context.body2,
-              cursorColor: context.primary,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//             FormBuilderTextField(
+//               name: 'email',
+//               decoration: InputDecoration(
+//                 suffixIcon: Icon(Icons.construction),
+//                 prefixIcon: Icon(Icons.construction),
+//                 labelText: 'Email',
+//                 hintText: 'you@example.com',
+//                 filled: true,
+//                 fillColor: context.neutral200,
+//                 border: OutlineInputBorder(
+//                   borderRadius: BorderRadius.circular(8.r),
+//                   borderSide: BorderSide(color: context.neutral300),
+//                 ),
+//                 focusedBorder: OutlineInputBorder(
+//                   borderRadius: BorderRadius.circular(8.r),
+//                   borderSide: BorderSide(color: context.primary300, width: 2),
+//                 ),
+//               ),
+//               style: context.body2,
+//               cursorColor: context.primary,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
