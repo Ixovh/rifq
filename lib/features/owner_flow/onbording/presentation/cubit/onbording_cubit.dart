f80afbe592236e7dd78ed0742ac29f26f6574f49
shatha Hammad <Shatha.hammadalblawi@gmail.com>
2025-11-23 20:32:01 +0300

    ui /Cubit (Text Button Skip ) Done,

--- lib/features/owner_flow/onbording/presentation/cubit/onbording_cubit.dart
+++ lib/features/owner_flow/onbording/presentation/cubit/onbording_cubit.dart
@@ -1,8 +1,24 @@
+import 'dart:math';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'onbording_state.dart';
 
-class OnbordingCubit extends Cubit<OnbordingState> {
-  OnbordingCubit() : super(OnbordingInitial());
+class OnbordingCubit extends Cubit<int> {
+  OnbordingCubit() : super(0);
+
+
+  void nextPage(){
+    if(state<2){
+      emit(state +1);
+    }
+  }
+
+  void skip(){
+    emit(2);
+  }
+  void changePage(int index){
+    emit(index);
+  }
 }
--- lib/features/owner_flow/onbording/presentation/widgets/custome_container_widgets.dart
+++ lib/features/owner_flow/onbording/presentation/widgets/custome_container_widgets.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:rifq/features/owner_flow/onbording/presentation/cubit/onbording_cubit.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import '../../../../../core/theme/app_color.dart';
 import 'custome_button_widgets.dart';
 
 class CustomeContainerWidgets extends StatelessWidget{
   final String title;
   final String subTitle;
-  const CustomeContainerWidgets({super.key, required this.title, required this.subTitle});
+  final int pageIndex;
+  final PageController controller;
+  const CustomeContainerWidgets({
+    super.key, required this.title,
+    required this.subTitle,
+    required this.pageIndex,
+    required this.controller,
+  });
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,34 +34,74 @@ class CustomeContainerWidgets extends StatelessWidget{
           borderRadius: BorderRadius.only(topLeft:Radius.circular(50),topRight:Radius.circular(50),)
       ),
       child: SingleChildScrollView(
+        // physics: NeverScrollableScrollPhysics(),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Text(title,style: TextStyle(color: Color(0xFF4A4A4A),fontSize: 32.sp,fontWeight: FontWeight.w500),),
-            SizedBox(height: 16.h),
-            Text(subTitle,style: TextStyle(color: Color(0xFF606060),fontSize: 18.sp,fontWeight: FontWeight.w400)),
-            SizedBox(height: 80.h,),
-            CustomeButtonWidgets
-              (titel: 'Next',
-              onPressed: () { },
-              buttonWidth: 366.w,
-              buttonhight: 58.h,),
-            SizedBox(height: 16.h),
-            TextButton(onPressed: (){},
-                child: Text('Skip',style: TextStyle(color: Color(0xFF777777),fontSize: 20.sp,fontWeight: FontWeight.w500),))
+            SmoothPageIndicator(
+              controller: controller,
+              count: 3,
+              effect: CustomizableEffect(
+                spacing: 12,
+                dotDecoration: DotDecoration(
+                  width: 18.w,
+                  height: 18.w,
+                  color: Colors.white,
+                  dotBorder: DotBorder(
+                    color: AppColors.primary200,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                activeDotDecoration: DotDecoration(
+                  width: 22,
+                  height: 22,
+                  color:AppColors.primary200,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+            ),
+
+            Text(title,style: TextStyle(color: AppColors.neutral900,fontSize:32.sp,fontWeight: FontWeight.w500)),
+            SizedBox(height: 18.h),
+            Text(subTitle,style: TextStyle(color: AppColors.neutral800,fontSize:18.sp,fontWeight: FontWeight.w400)),
+            SizedBox(height: 24.h,),
+            if(pageIndex == 2) // last page
+              CustomeButtonWidgets(
+                titel: 'Get Started',
+                onPressed: () { },
+                buttonWidth: 366.w,
+                buttonhight: 58.h,
+              )
+            else ...[
+              CustomeButtonWidgets(
+                titel: 'Next',
+                onPressed: () {
+                  print("object  $pageIndex");
+                  context.read<OnbordingCubit>().nextPage();
+                  controller.nextPage(duration: Duration(milliseconds: 300),
+                      curve: Curves.ease);
+                },
+                buttonWidth: 366.w,
+                buttonhight: 58.h,
+              ),
+              SizedBox(height: 18.h),
+              TextButton(
+                onPressed: (){
+                  print("skiip  $pageIndex");
+
+                  context.read<OnbordingCubit>().skip();
+                  controller.jumpToPage(2);
+                },
+                child: Text('Skip',style: TextStyle(color: Color(0xFF777777),fontSize: 20.sp,fontWeight: FontWeight.w500), ),
+              ),
+            ],
           ],
 
-        ),
-      ),
+    )));
 
-    );
 
   }
 }
-
-
-
-
 
 
 
