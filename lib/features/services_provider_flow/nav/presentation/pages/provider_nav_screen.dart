eb13157315dabf2a21527d72fed5ed421e9a7798
Hatem Ayed <ixovhh@gmail.com>
2025-12-15 01:15:44 +0300

    provider flow is reeady to use

--- lib/features/services_provider_flow/nav/presentation/pages/provider_nav_screen.dart
+++ lib/features/services_provider_flow/nav/presentation/pages/provider_nav_screen.dart
@@ -8,93 +8,94 @@ import 'package:rifq/features/services_provider_flow/auth/data/datasources/provi
 import 'package:rifq/features/services_provider_flow/home/data/datasources/reservation_data_source.dart';
 import 'package:rifq/features/services_provider_flow/home/domain/usecases/reservation_usecase.dart';
 import 'package:rifq/features/services_provider_flow/home/presentation/cubit/home_cubit.dart';
-import 'package:rifq/features/services_provider_flow/home/presentation/pages/provider_home_screen.dart';
 import 'package:rifq/features/services_provider_flow/nav/presentation/cubit/provider_nav_cubit.dart';
 import 'package:rifq/features/services_provider_flow/nav/presentation/cubit/provider_nav_state.dart';
-import 'package:rifq/features/services_provider_flow/profile/presentation/pages/provider_profile_screen.dart';
+import 'package:rifq/features/services_provider_flow/nav/presentation/widget/build_screen.dart';
+import 'package:rifq/features/services_provider_flow/profile/domain/usecases/provider_profile_usecase.dart';
+import 'package:rifq/features/services_provider_flow/profile/presentation/cubit/provider_profile_cubit.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProviderNavScreen extends StatelessWidget {
   const ProviderNavScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Builder(
-      builder: (context) {
-        final cubit = context.read<ProviderNavCubit>();
-        return BlocBuilder<ProviderNavCubit, ProviderNavState>(
-          builder: (context, state) {
-            return Scaffold(
-              resizeToAvoidBottomInset: true,
-              backgroundColor: context.background,
-              bottomNavigationBar: BottomNavigationBar(
-                backgroundColor: context.background,
-                type: BottomNavigationBarType.fixed,
-                currentIndex: cubit.currentIndex,
-                onTap: (value) {
-                  cubit.changeIndex(index: value);
-                },
-                selectedItemColor: context.primary,
-                unselectedItemColor: context.neutral500,
-                selectedLabelStyle: context.body3.copyWith(
-                  fontWeight: FontWeight.w600,
-                ),
-                unselectedLabelStyle: context.body3.copyWith(
-                  fontWeight: FontWeight.w400,
-                ),
-                items: [
-                  BottomNavigationBarItem(
-                    label: "Requests",
-                    icon: SvgPicture.asset(
-                      'assets/icon/notification-status.svg',
-                      width: 24.w,
-                      height: 24.h,
-                      colorFilter: ColorFilter.mode(
-                        cubit.currentIndex == 0
-                            ? context.primary
-                            : context.neutral500,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                  ),
-                  BottomNavigationBarItem(
-                    label: "Profile",
-                    icon: SvgPicture.asset(
-                      'assets/icon/frame.svg',
-                      width: 24.w,
-                      height: 24.h,
-                      colorFilter: ColorFilter.mode(
-                        cubit.currentIndex == 1
-                            ? context.primary
-                            : context.neutral500,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              body: _buildScreen(context, cubit.currentIndex),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  Widget _buildScreen(BuildContext context, int index) {
-    switch (index) {
-      case 0:
-        return BlocProvider(
+    final authId = Supabase.instance.client.auth.currentUser?.id ?? '';
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
           create: (context) => HomeCubit(
             getIt.get<ReservationUseCase>(),
             getIt.get<ProviderBaseAuthDataSource>(),
             getIt.get<BaseReservationDataSource>(),
           )..loadHomeData(),
-          child: const ProviderHomeScreen(),
-        );
-      case 1:
-        return const ProviderProfileScreen();
-      default:
-        return const SizedBox.shrink();
-    }
+        ),
+        BlocProvider(
+          create: (context) =>
+              ProviderProfileCubit(getIt.get<ProviderProfileUsecase>())
+                ..getProviderProfile(authId),
+        ),
+      ],
+      child: Builder(
+        builder: (context) {
+          final cubit = context.read<ProviderNavCubit>();
+          return BlocBuilder<ProviderNavCubit, ProviderNavState>(
+            builder: (context, state) {
+              return Scaffold(
+                resizeToAvoidBottomInset: true,
+                backgroundColor: context.background,
+                bottomNavigationBar: BottomNavigationBar(
+                  backgroundColor: context.background,
+                  type: BottomNavigationBarType.fixed,
+                  currentIndex: cubit.currentIndex,
+                  onTap: (value) {
+                    cubit.changeIndex(index: value);
+                  },
+                  selectedItemColor: context.primary,
+                  unselectedItemColor: context.neutral500,
+                  selectedLabelStyle: context.body3.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                  unselectedLabelStyle: context.body3.copyWith(
+                    fontWeight: FontWeight.w400,
+                  ),
+                  items: [
+                    BottomNavigationBarItem(
+                      label: "Requests",
+                      icon: SvgPicture.asset(
+                        'assets/icon/notification-status.svg',
+                        width: 24.w,
+                        height: 24.h,
+                        colorFilter: ColorFilter.mode(
+                          cubit.currentIndex == 0
+                              ? context.primary
+                              : context.neutral500,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                    ),
+                    BottomNavigationBarItem(
+                      label: "Profile",
+                      icon: SvgPicture.asset(
+                        'assets/icon/frame.svg',
+                        width: 24.w,
+                        height: 24.h,
+                        colorFilter: ColorFilter.mode(
+                          cubit.currentIndex == 1
+                              ? context.primary
+                              : context.neutral500,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                body: BuildScreen(context: context, index: cubit.currentIndex),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
