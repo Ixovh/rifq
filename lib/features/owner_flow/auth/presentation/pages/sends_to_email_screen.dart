48572909e4b724189f203830b1e0c7c8806eb7ef
Hatem Ayed <ixovhh@gmail.com>
2025-12-17 20:29:40 +0300

    auth loading is done

--- lib/features/owner_flow/auth/presentation/pages/sends_to_email_screen.dart
+++ lib/features/owner_flow/auth/presentation/pages/sends_to_email_screen.dart
@@ -8,7 +8,6 @@ import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rifq/core/routes/base_routes.dart';
 import 'package:rifq/core/theme/app_theme.dart';
-import 'package:rifq/core/common/widgets/lottie_loading/lottie_loding.dart';
 import 'package:rifq/features/owner_flow/auth/presentation/cubit/auth_cubit.dart';
 import 'package:rifq/features/owner_flow/auth/presentation/widgets/container_button.dart';
 import 'package:rifq/features/owner_flow/auth/presentation/widgets/custom_bottom_sheet.dart';
@@ -22,7 +21,7 @@ class SendsToEmailScreen extends StatelessWidget {
     return Builder(
       builder: (context) {
         final cubit = context.read<AuthCubit>();
-        return BlocListener<AuthCubit, AuthState>(
+        return BlocConsumer<AuthCubit, AuthState>(
           listener: (context, state) {
             switch (state) {
               case AuthPasswordResetEmailSentState _:
@@ -31,102 +30,99 @@ class SendsToEmailScreen extends StatelessWidget {
                   extra: {"cubit": cubit, "isPassword": true},
                 );
                 break;
-              case AuthLoadingState _:
-                LottieLoding();
-                break;
               case AuthErrorState _:
                 ScaffoldMessenger.of(
                   context,
                 ).showSnackBar(SnackBar(content: Text(state.msg)));
                 break;
               default:
-                LottieLoding();
                 break;
             }
           },
-          child: Scaffold(
-            backgroundColor: context.neutral100,
-            resizeToAvoidBottomInset: false,
-            bottomSheet: CustomBottomSheet(
-              content: Column(
-                mainAxisAlignment: .center,
-                crossAxisAlignment: .start,
-                children: [
-                  Center(
-                    child: Text(
-                      'Reset Password',
-                      style: context.h5.copyWith(
-                        fontSize: 24.sp,
-                        fontWeight: FontWeight.w500,
-                        color: context.primary400,
+          builder: (context, state) {
+            final isLoading = state is AuthLoadingState;
+            return Scaffold(
+              backgroundColor: context.neutral100,
+              resizeToAvoidBottomInset: false,
+              bottomSheet: CustomBottomSheet(
+                content: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Text(
+                        'Reset Password',
+                        style: context.h5.copyWith(
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.w500,
+                          color: context.primary400,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 8.h),
-
-                  Text(
-                    'Enter your email to verify and reset your password.',
-                    style: context.body2.copyWith(color: context.neutral800),
-                  ),
-                  SizedBox(height: 24.h),
-                  FormBuilder(
-                    key: cubit.resetVerfiyEmailFormKey,
-                    child: CustomFormBuilderTextField(
-                      name: 'email',
-                      label: 'Email',
-                      iconData: CupertinoIcons.mail_solid,
-                      controller: cubit.resetEmailController,
-                      validators: [
-                        FormBuilderValidators.required(
-                          errorText: '(e.g., username@example.com).',
-                        ),
-                        FormBuilderValidators.email(
-                          errorText: '(e.g., username@example.com).',
-                        ),
-                      ],
+                    SizedBox(height: 8.h),
+                    Text(
+                      'Enter your email to verify and reset your password.',
+                      style: context.body2.copyWith(color: context.neutral800),
                     ),
-                  ),
-                  SizedBox(height: 32.h),
-
-                  ContainerButton(
-                    label: 'verfiy',
-                    containerColor: context.primary300,
-                    textColor: context.neutral100,
-                    fontSize: 20,
-                    onTap: () async {
-                      if (cubit.resetVerfiyEmailFormKey.currentState
-                              ?.saveAndValidate() ??
-                          false) {
-                        await cubit.sendPasswordResetEmail(
-                          email: cubit.resetEmailController.text,
-                        );
-                      }
-                    },
-                  ),
-                  SizedBox(height: 12.h),
-
-                  ContainerButton(
-                    label: 'Cancel',
-                    containerColor: context.neutral100,
-                    textColor: context.primary300,
-                    fontSize: 20,
-                    onTap: () {
-                      if (context.canPop()) {
-                        context.pop();
-                      }
-                    },
-                  ),
-                  Spacer(),
-                ],
+                    SizedBox(height: 24.h),
+                    FormBuilder(
+                      key: cubit.resetVerfiyEmailFormKey,
+                      child: CustomFormBuilderTextField(
+                        name: 'email',
+                        label: 'Email',
+                        iconData: CupertinoIcons.mail_solid,
+                        controller: cubit.resetEmailController,
+                        validators: [
+                          FormBuilderValidators.required(
+                            errorText: '(e.g., username@example.com).',
+                          ),
+                          FormBuilderValidators.email(
+                            errorText: '(e.g., username@example.com).',
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 32.h),
+                    ContainerButton(
+                      label: 'verfiy',
+                      containerColor: context.primary300,
+                      textColor: context.neutral100,
+                      fontSize: 20,
+                      isLoading: isLoading,
+                      onTap: () async {
+                        if (cubit.resetVerfiyEmailFormKey.currentState
+                                ?.saveAndValidate() ??
+                            false) {
+                          await cubit.sendPasswordResetEmail(
+                            email: cubit.resetEmailController.text,
+                          );
+                        }
+                      },
+                    ),
+                    SizedBox(height: 12.h),
+                    ContainerButton(
+                      label: 'Cancel',
+                      containerColor: context.neutral100,
+                      textColor: context.primary300,
+                      fontSize: 20,
+                      onTap: () {
+                        if (context.canPop()) {
+                          context.pop();
+                        }
+                      },
+                    ),
+                    Spacer(),
+                  ],
+                ),
               ),
-            ),
-            body: SafeArea(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: SvgPicture.asset('assets/icon/logo.svg'),
+              body: SafeArea(
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset('assets/icon/logo.svg'),
+                ),
               ),
-            ),
-          ),
+            );
+          },
         );
       },
     );
